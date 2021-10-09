352b4e2479bc5e552c96039db693defa07235828
Amogh Prabhu <amoghprabhu@hotmail.com>
2021-10-09 12:36:08 +0530

    Text field and button added to login page

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -4,10 +4,45 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Column(
-      children: [
-        Center(child: Image.asset("assets/images/login_image.png")),
-      ],
-    ));
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/images/login_image.png"),
+          const SizedBox(height: 20),
+          const Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Enter username",
+                    label: Text("Username"),
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "Enter password",
+                    label: Text("Password"),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 25),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Login"),
+          ),
+        ],
+      ),
+    );
   }
 }
