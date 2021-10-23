20dae6a6d6b6cdbfe9cfda916c23f68733f7fe31
Amogh Prabhu <amoghprabhu@hotmail.com>
2021-10-23 17:46:27 +0530

    Added clipping to container

--- lib/Pages/home_detail_page.dart
+++ lib/Pages/home_detail_page.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:test_s/widgets/theme.dart';
 
@@ -15,18 +19,64 @@ class HomeDetailPage extends StatelessWidget {
       appBar: AppBar(),
       backgroundColor: MyTheme.creamColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(32.0),
+        child: Container(
+          width: double.infinity,
           child: Column(
             children: [
               Hero(
                 tag: Key(catalogItem.id.toString()),
-                child: Image.network(catalogItem.image),
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  height: 300,
+                  child: Image.network(catalogItem.image),
+                ),
               ),
+              Expanded(
+                child: ClipPath(
+                  clipper: ArcClipper(),
+                  child: Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(50),
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        Text(
+                          catalogItem.name,
+                          textScaleFactor: 2,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          catalogItem.desc,
+                          textScaleFactor: 1.2,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
       ),
     );
   }
+}
+
+class ArcClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.lineTo(0, size.height);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.quadraticBezierTo(size.width * 0.5, size.height * 0.15, 0, 0);
+
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper old) => false;
 }
